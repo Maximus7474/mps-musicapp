7aa8c703ea35bd6cfa90c88706eac500ced473d7
Maximus7474 <the.maximus.7474@gmail.com>
2026-07-25 16:22:47 +0200

    feat(web/utils): generate gradient palette

--- web/src/utils/utils.ts
+++ web/src/utils/utils.ts
@@ -1,1 +1,19 @@
 export const devMode = !window?.['invokeNative'];
+
+const GRADIENT_PALETTES = [
+  '135deg, #a259ff, #6366f1',
+  '135deg, #f43f5e, #fb923c',
+  '135deg, #06b6d4, #3b82f6',
+  '135deg, #10b981, #06b6d4',
+  '135deg, #8b5cf6, #ec4899',
+  '135deg, #f59e0b, #ef4444',
+];
+
+export const generateGradient = (id: number | string): string => {
+  const index =
+    typeof id === 'number'
+      ? id % GRADIENT_PALETTES.length
+      : [...id].reduce((acc, char) => acc + char.charCodeAt(0), 0) % GRADIENT_PALETTES.length;
+
+  return GRADIENT_PALETTES[Math.abs(index)];
+};

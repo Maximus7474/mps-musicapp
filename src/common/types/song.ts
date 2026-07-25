b433671f10f915420087d42acaf696d0435d5d70
Maximus7474 <the.maximus.7474@gmail.com>
2026-07-25 15:11:10 +0200

    chore(common/types): extend song types with artist & playlist

--- src/common/types/song.ts
+++ src/common/types/song.ts
@@ -1,8 +1,23 @@
+/** duration in seconds */
 export type SongBasic = {
   id: number;
   name: string;
   author: string;
   liked: boolean;
   url: string;
+  image?: string;
+  duration?: number;
+};
+
+export type ArtistBasic = {
+  id: number;
+  name: string;
+  image?: string;
+};
+
+export type PlaylistBasic = {
+  id: number;
+  title: string;
+  tracks: number;
   image?: string;
 };

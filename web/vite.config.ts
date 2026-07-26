f3a70ce2bfb7c142bfd6be447f1d0dcef324b213
Maximus7474 <the.maximus.7474@gmail.com>
2026-07-26 20:05:51 +0200

    chore(web): create alias for paths within the web app using ~/

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +20,11 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['@communityox/ox_lib'],
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+      '@common': path.resolve(__dirname, '../src/common'),
+    },
   },
 });
